21fe689a2951701f6733e1bfb4311f8889d67668
ShahbozbekCoder <shahbozbey99@gmail.com>
2023-10-15 12:21:09 +0500

    beginning my new project

---
+++ app/src/main/java/uz/shahbozbek/namozvaqtlari/ui/splash/SplashScreen.kt
@@ -0,0 +1,12 @@
+package uz.shahbozbek.namozvaqtlari.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.shahbozbek.namozvaqtlari.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
