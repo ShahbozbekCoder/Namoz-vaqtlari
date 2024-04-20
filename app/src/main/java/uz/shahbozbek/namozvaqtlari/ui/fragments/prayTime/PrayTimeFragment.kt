d6f3ef3112dd10ebb613247d8d6efa2a2640dbe2
ShahbozbekCoder <shahbozbey99@gmail.com>
2024-04-20 17:58:17 +0500

    added nw viewModel

--- app/src/main/java/uz/shahbozbek/namozvaqtlari/ui/fragments/prayTime/PrayTimeFragment.kt
+++ app/src/main/java/uz/shahbozbek/namozvaqtlari/ui/fragments/prayTime/PrayTimeFragment.kt
@@ -7,16 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
 import uz.shahbozbek.namozvaqtlari.R
+import uz.shahbozbek.namozvaqtlari.databinding.FragmentSolatTimeBinding
 
 @AndroidEntryPoint
-class PrayTimeFragment : Fragment() {
+class PrayTimeFragment : Fragment(R.layout.fragment_solat_time) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_solat_time, container, false)
+    private lateinit var binding: FragmentSolatTimeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSolatTimeBinding.bind(view)
     }
 
 }
